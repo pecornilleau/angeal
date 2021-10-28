2ee5608e65af104f499513605a70181108ba198e
pec <pe.cornilleau@gmail.com>
2021-10-28 14:51:04 +0200

    first try

---
+++ lib/mtprint.ml
@@ -0,0 +1,16 @@
+open Mtree
+open Format
+let pprint t = 
+    let rec loop n t =
+        let spaces = String.make (2*n) ' ' in
+        match t with 
+            | L i ->        printf "%s%d\n" spaces i
+            | Nu (_,t') ->  printf "%s%c " spaces '-'; 
+                            loop 0  t';
+                            (* printf "%s%c\n" spaces '/' *)
+            | N(_,t1,t2) ->
+                            loop (n+1)  t1;
+                            printf "%s%c\n" spaces '<'; 
+                            loop (n+1)  t2
+    in
+    loop 0 t    
---
+++ test/exec/show.ml
@@ -0,0 +1,26 @@
+open Angeal
+
+open Mtree
+let show t = 
+    Format.printf "----------------\n";
+    Mtprint.pprint t;
+    Format.printf "-> %b\n" (check t)
+
+let tree_s_ok = Nu (hash (L 1), L 1)
+let tree_d_ok = N (hash tree_s_ok + hash (L 1), tree_s_ok, L 1)
+
+let tree_d_ok2 = N (hash tree_s_ok + hash (L 2), tree_s_ok, L 2)
+
+let _ = show  (build tree_d_ok tree_d_ok2)
+
+
+let _ = show (r_build [1;2;3;4])
+let _ = show (r_build [1;2;3;4])
+let _ = show (r_build [1;2;3;4])
+
+let rec upto n = if n == 0 then [0] else n::(upto (n-1))
+    
+let _ = show (r_build (upto 16))
+let _ = show (r_build (upto 16))
+let _ = show (r_build (upto 16))
+let _ = show (r_build (upto 16))
